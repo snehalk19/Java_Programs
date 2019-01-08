c96b45daa391b4e5fa2a3b30898d59ba7c22b9d4
Snehal Khot <39563593+snehalk19@users.noreply.github.com>
2019-01-08 20:51:15 +0530

    Add files via upload

---
+++ Queue_Operations.java
@@ -0,0 +1,82 @@
+/* Problem Statement
+Given N integers, the task is to insert those elements in the queue. Also, given M integers, task is to find the frequency of each number in the Queue.
+*/
+
+
+{
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+//Position this line where user code will be pasted.
+// Driver class with driver code
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+		
+		// Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    Queue<Integer> q = new LinkedList<>();
+		    int n = sc.nextInt();
+		    
+		    // Invoking object of Geeks class
+		    Geeks obj = new Geeks();
+		    
+		    for(int i = 0;i<n;i++){
+		        int k = sc.nextInt();
+		        obj.insert(q, k);
+		    }
+		   
+		    int x = sc.nextInt();
+		    for(int i = 0;i<x;i++){
+		        int k = sc.nextInt();
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            System.out.println(f);
+		        }
+		        else{
+		            System.out.println("-1");
+		        }
+		    }
+		}
+	}
+}
+}
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//User function Template for Java
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks{
+    
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        
+        // Your code here
+        q.add(k);
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        int freq = 0;
+        for(Integer item:q)
+        {
+            if(item==k)
+            {
+                freq++;
+            }
+        }
+        // Your code here
+     return freq;   
+    }
+    
+}
---
+++ stack_operation.java
@@ -0,0 +1,117 @@
+/*
+Problem Statement
+Given a stack of integers and Q queries. The task is to perform operation on stack according to the query.
+nput Format:
+First line of input contains nubmer of testcases T. For each testcase, first line of input contains Q, number of queries. Next line contains Q queries seperated by space. Queries are as:
+
+i x : (adds element x in the stack).
+
+r : (returns and removes the topmost element from the stack).
+
+h : Prints the topmost element.
+
+f y : (check if the element y is present or not in the stack). Print "Yes" if present, else "No".
+*/
+
+{
+//Initial Template for Java
+import java.util.*;
+//Position this line where user code will be pasted.
+// Driver class with Driver code
+class GFG 
+{
+	// Driver code
+	public static void main (String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0)
+		{
+		    // Declaring stack of integers
+		    Stack <Integer> st = new Stack<Integer>();
+		    
+		    int q = sc.nextInt();
+		    
+		    // Working with queries
+		    while(q-- > 0)
+		    {
+		        char ch = sc.next().charAt(0);
+		        Geeks obj = new Geeks();
+		       if(ch == 'i')
+		      {
+		         int x = sc.nextInt();
+		         obj.insert(st, x);
+		      }
+		    
+		      if(ch == 'r')
+		      {
+		          obj.remove(st);
+		      }
+		      
+		      if(ch == 'h')
+		      {
+		          obj.headOf_Stack(st);
+		      }
+		      if(ch == 'f')
+		      {
+		          int x = sc.nextInt();
+		          obj.find(st, x);
+		      }
+		      
+		      
+		    }
+		    // System.out.println();
+		 }
+	}
+}
+}
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//User function Template for Java
+// Helper class to implement functions
+// insert : to insert element to stack
+// remove : to pop element from stack
+// headOf_Stack : to return top of stack
+// find : to find the element in stack
+class Geeks
+{
+    // Function to insert element to stack
+    public static void insert(Stack<Integer> st, int x)
+    {
+        // Your code here
+        st.push(x);
+    }
+    
+    // Function to pop element from stack
+    public static void remove(Stack<Integer> st)
+    {
+        int x = st.pop();// Your code here
+        
+    }
+    
+    // Function to return top of stack
+    public static void headOf_Stack(Stack<Integer> st)
+    {
+        int x = st.peek();// Your code here
+        System.out.println(x + " ");
+    }
+    
+    // Function to find the element in stack
+    public static void find(Stack<Integer> st, int val)
+    {
+    
+        if(st.search(val)!= -1)
+        {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+    }
+}
